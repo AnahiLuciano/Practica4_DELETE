da204989387fee9d8b0773aeab4d1599a395adc4
AnahiLuciano <79292946+AnahiLuciano@users.noreply.github.com>
2021-03-01 20:56:25 -0600

    Add files via upload
    Archivos de la practica 4 DELETE

---
+++ ListaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\estudiantes; //Modelo
+
+class ListaController extends Controller
+{
+    public function index(Request $request)
+    {
+        $estudiante= estudiantes::all();
+        return view ('VistaEstudiantes', compact('estudiante')); 
+        
+       /* $texto=trim($request->get ('texto'));
+        $estudiante=DB::table ('estudiantes')-> select('Matricula','Nombre','Direccion')
+        ->where ('Matricula','=',$texto)->paginate(10);
+        return view ('VistaEstudiantes', compact('estudiante'));*/
+
+    }
+    public function destroy($id)
+    {
+    $estudiante= estudiantes::find($id);
+    //dd($estudiante);
+    $estudiante->delete();
+    return redirect()->route('Lista.index');
+    }
+    
+}
